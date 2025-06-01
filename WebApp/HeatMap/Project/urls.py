680763880a8a213a1498884898ea7516d53faf81
SkarBS27 <ofbuenanosantos27@gmail.com>
2025-06-01 04:08:43 +0200

    Update urls.py
    Update urls

--- WebApp/HeatMap/Project/urls.py
+++ WebApp/HeatMap/Project/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('api/sensor_data/', views.get_sensor_data, name='get_sensor_data'),
-    path('api/sensors/', views.get_sensor_data), 
-    path('api/receive_sensor_data/', views.receive_sensor_data, name='receive_sensor_data'),
+path('', views.index, name='index'),
+path('api/data/', views.receive_sensor_data, name='receive_sensor_data'),
+path('api/sensor-data/', views.get_sensor_data, name='get_sensor_data'),
+path('api/ping/', views.ping_view, name='ping'),
+path('api/view-data-file/', views.view_data_file, name='view_data_file'),
 ]
