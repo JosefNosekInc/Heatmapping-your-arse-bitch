434efddaacea1252ad14c39f4a8a898f2e335e85
LACMAGO PEPA ABED NEGO <lacmagopepa.abednego@gmail.com>
2025-06-06 07:25:53 +0200

    Update models.py

--- WebApp/HeatMap/Project/models.py
+++ WebApp/HeatMap/Project/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class SensorData(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
+    node_id = models.IntegerField()
     x = models.FloatField()
     y = models.FloatField()
     temperature = models.FloatField()
+    humidity = models.FloatField()
 
     def __str__(self):
-        return f"({self.x}, {self.y}) = {self.temperature}°C at {self.timestamp}"
+        return f"Node {self.node_id} - Temp: {self.temperature}°C, Humidity: {self.humidity}% at {self.timestamp}"
